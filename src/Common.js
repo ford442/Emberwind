b27f40af25566f2c6af18b19fe4edd4d7ca51f6f
Noah Wolfe Cohn <noahc42@gmail.com>
2021-10-13 00:10:58 -0700

    clion reformat

--- src/Common.js
+++ src/Common.js
@@ -1,7 +1,6 @@
 /*
  * Common.js module contains exception classes and utility methods
  */
-
 /**
  * Clamps the given value based on given parameters
  *
@@ -10,25 +9,18 @@
  * @param {number} max Maximum value
  * @returns {number}
  */
-function Clamp(min, val, max) {
-  return Math.min(max, Math.max(min, val));
+function Clamp(min,val,max){
+return Math.min(max,Math.max(min,val));
 }
-
-
-function lerp(from, to, t) {
-	return from + t * (to - from);
+function lerp(from,to,t){
+return from + t * (to - from);
 }
-
 // ---------------------------------------------------------------------------
-
-
-function Size2(w, h){
-	this.w = w;
-	this.h = h;
+function Size2(w,h){
+this.w = w;
+this.h = h;
 }
-
 // Point2  -------------------------------------------------------------------
-
 /**
  * Represents a point in a two dimensional space. Arithmetic is performed in place.
  *
@@ -36,72 +28,56 @@ function Size2(w, h){
  * @param {number} y The y coordinate
  * @constructor
  */
-function Point2(x, y) {
-    this.x = x;
-    this.y = y;
+function Point2(x,y){
+this.x = x;
+this.y = y;
 }
-
-Point2.prototype.MagnitudeSquared = function () {
-    return this.x * this.x + this.y * this.y;
+Point2.prototype.MagnitudeSquared = function(){
+return this.x * this.x + this.y * this.y;
 };
-
-Point2.prototype.Magnitude = function () {
-    return Math.sqrt(this.MagnitudeSquared());
+Point2.prototype.Magnitude = function(){
+return Math.sqrt(this.MagnitudeSquared());
 };
-
-Point2.prototype.add = function (v) {
-    this.x += v.x;
-    this.y += v.y;
-    return this;
+Point2.prototype.add = function(v){
+this.x += v.x;
+this.y += v.y;
+return this;
 };
-
-Point2.prototype.addNew = function (v) {
-    return new Point2(this.x + v.x, this.y + v.y);
+Point2.prototype.addNew = function(v){
+return new Point2(this.x + v.x,this.y + v.y);
 };
-
-Point2.prototype.sub = function (v) {
-    this.x -= v.x;
-    this.y -= v.y;
-    return this;
+Point2.prototype.sub = function(v){
+this.x -= v.x;
+this.y -= v.y;
+return this;
 };
-
-Point2.prototype.subNew = function (v) {
-    return new Point2(this.x - v.x, this.y - v.y);
+Point2.prototype.subNew = function(v){
+return new Point2(this.x - v.x,this.y - v.y);
 };
-
-Point2.prototype.mul = function (f) {
-    this.x *= f;
-    this.y *= f;
-    return this;
+Point2.prototype.mul = function(f){
+this.x *= f;
+this.y *= f;
+return this;
 };
-
-Point2.prototype.mulNew = function (f) {
-    return new Point2(this.x * f, this.y * f);
+Point2.prototype.mulNew = function(f){
+return new Point2(this.x * f,this.y * f);
 };
-
-Point2.prototype.div = function (f) {
-    this.x /= f;
-    this.y /= f;
-    return this;
+Point2.prototype.div = function(f){
+this.x /= f;
+this.y /= f;
+return this;
 };
-
-Point2.prototype.eq = function (v) {
-    return this.x === v.x && this.y === v.y;
+Point2.prototype.eq = function(v){
+return this.x === v.x && this.y === v.y;
 };
-
-Point2.prototype.neq = function (v) {
-    return this.x !== v.x || this.y !== v.y;
+Point2.prototype.neq = function(v){
+return this.x !== v.x || this.y !== v.y;
 };
-
-Point2.prototype.copy = function () {
-    return new Point2(this.x, this.y);
+Point2.prototype.copy = function(){
+return new Point2(this.x,this.y);
 };
-
 // ---------------------------------------------------------------------------
-
-
 // Vec2 ----------------------------------------------------------------------
-
 /**
  * Two dimensional vector class with accompanying functions for manipulating
  * points. Extends the Point2 class
@@ -111,33 +87,25 @@ Point2.prototype.copy = function () {
  * @constructor
  * @auguments Point2
  */
-function Vec2() {
-    Point2.apply(this, arguments);
+function Vec2(){
+Point2.apply(this,arguments);
 }
-
 Vec2.prototype = new Point2();
-
-Vec2.prototype.Dot = function (arg) {
-    return this.x * arg.x + this.y * arg.y;
+Vec2.prototype.Dot = function(arg){
+return this.x * arg.x + this.y * arg.y;
 };
-
-Vec2.prototype.Cross = function (arg) {
-    return this.x * arg.y - this.y * arg.x;
+Vec2.prototype.Cross = function(arg){
+return this.x * arg.y - this.y * arg.x;
 };
-
-Vec2.prototype.Normalize = function () {
-    var inv = 1 / Math.sqrt(this.x * this.x + this.y * this.y);
-    this.x *= inv;
-    this.y *= inv;
-
-    return this;
+Vec2.prototype.Normalize = function(){
+var inv = 1 / Math.sqrt(this.x * this.x + this.y * this.y);
+this.x *= inv;
+this.y *= inv;
+return this;
 };
-
-Vec2.prototype.copy = function () {
-    return new Vec2(this.x, this.y);
+Vec2.prototype.copy = function(){
+return new Vec2(this.x,this.y);
 };
-
-
 /*
 function Vec2.XAxis() {
     return new Vec2(1, 0);
@@ -151,10 +119,7 @@ function Vec2.Zero() {
     return nev Vec2(0, 0);
 }
 */
-
 // ---------------------------------------------------------------------------
-
-
 /**
  * Randomizes a number in the specifed range.
  *
@@ -162,135 +127,122 @@ function Vec2.Zero() {
  * @param {number} h higher limit of the range.
  * @returns {number}
  */
-function randomRange(l, h){
-	return l + Math.random() * (h-l);
+function randomRange(l,h){
+return l + Math.random() * (h - l);
 }
-
 /**
  * Randomizes a boolean.
  *
  * @returns {Boolean}
  */
 function randomBool(){
-    return Math.random() >= 0.5;
+return Math.random() >= 0.5;
 }
-
 /**
  * Copies all properties from the source to the destination.
- * 
+ *
  * @param {Object} source the source.
  * @param {Object} destination the destination.
  */
-function copy(source, destination) {
-	for ( var k in source) {
-		if (source.hasOwnProperty(k)) destination[k] = source[k];
-	}
+function copy(source,destination){
+for(var k in source){
+if(source.hasOwnProperty(k)){
+destination[k] = source[k];
 }
-
+}
+}
 /**
  * Creates a callback function with the correct "this".
- * 
+ *
  * @param {Function} callback the callback function to be called.
  * @param {Object} that the wanted "this".
  * @returns {Function} a proper callback function.
  */
-function createCallback(callback, that) {
-	return function() {
-		callback.apply(that, arguments);
-	};
+function createCallback(callback,that){
+return function(){
+callback.apply(that,arguments);
+};
 }
-
 /**
  * Creates a callback function with the correct "this".
  * Takes additiional arguments that will be added to the beginning of the
  * argument list that will be sent to the callback.
- * 
+ *
  * @param {Function} callback the callback function to be called.
  * @param {Object} that the wanted "this".
  * @returns {Function} a proper callback function.
  */
-function createCallbackWithArgs(callback, that) {
-    var arg = arguments;
-    delete arg[0];
-    delete arg[1];
-    return function() {
-        callback.apply(that, arg.concat(arguments));
-    };
+function createCallbackWithArgs(callback,that){
+var arg = arguments;
+delete arg[0];
+delete arg[1];
+return function(){
+callback.apply(that,arg.concat(arguments));
+};
 }
-
 /*
  * RuntimeException class
  */
-
 RuntimeException.prototype = new Error();
 RuntimeException.prototype.constructor = RuntimeException;
-
 /**
  * Base exception class
- * 
+ *
  * @param message is an optional error description
  * @returns {Exception}
  */
-function RuntimeException(message) {
-	this.assign(message);
+function RuntimeException(message){
+this.assign(message);
 }
-
 /**
  * creates message property and assign value to it. In case of a missing text,
  * empty string is assigned.
- * 
+ *
  * @param message is an optional error text
  */
-RuntimeException.prototype.assign = function(message) {
-	if (message === undefined) {
-		this.message = "";
-	} else {
-		this.message = message;
-	}
+RuntimeException.prototype.assign = function(message){
+if(message === undefined){
+this.message = "";
+}else{
+this.message = message;
+}
 };
-
 /*
  * AssertionError class
  */
-
 AssertionError.prototype = new RuntimeException();
 AssertionError.prototype.constructor = AssertionError;
-
 /**
  * AssertionError stands for an assertion failure
- * 
+ *
  * @param message is an optional message
  * @returns {AssertionError}
  */
-function AssertionError(message) {
-	this.assign(message);
+function AssertionError(message){
+this.assign(message);
 }
-
 /**
  * asserts on a condition
- * 
+ *
  * @param condition is a boolean value
  * @param message is an optional error description
  */
-function assert(condition, message) {
-	if (!condition) {
-		throw new AssertionError(message);
-	}
+function assert(condition,message){
+if(!condition){
+throw new AssertionError(message);
 }
-
+}
 /*
  * Exception class
  */
-
 Exception.prototype = new RuntimeException();
 Exception.prototype.constructor = Exception;
-
 /**
  * Exception stands for a recoverable exception
- * 
+ *
  * @param message is an optional message
  * @returns {Exception}
  */
-function Exception(message) {
-	this.assign(message);
+function Exception(message){
+this.assign(message);
 }
